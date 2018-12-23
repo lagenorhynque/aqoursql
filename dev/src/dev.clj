5e62c3475c33eade6b25996feca150eb9f7d7172
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-12-23 18:19:53 +0900

    Implement GraphQL API base

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,23 +1,52 @@
 (ns dev
   (:refer-clojure :exclude [test])
-  (:require [clojure.repl :refer :all]
-            [fipp.edn :refer [pprint]]
+  (:require [clojure.java.io :as io]
+            [clojure.repl :refer :all]
+            [clojure.spec.alpha :as s]
             [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.java.io :as io]
+            [com.walmartlabs.lacinia :as lacinia]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl]
             [eftest.runner :as eftest]
+            [fipp.edn :refer [pprint]]
             [integrant.core :as ig]
-            [integrant.repl :refer [clear halt go init prep reset]]
-            [integrant.repl.state :refer [config system]]))
+            [integrant.repl :refer [clear halt go init prep]]
+            [integrant.repl.state :refer [config system]]
+            [orchestra.spec.test :as stest]))
 
 (duct/load-hierarchy)
 
 (defn read-config []
   (duct/read-config (io/resource "dev.edn")))
 
-(defn test []
-  (eftest/run-tests (eftest/find-tests "test")))
+(defn reset []
+  (let [result (integrant.repl/reset)]
+    (with-out-str (stest/instrument))
+    result))
+
+;;; unit testing
+
+(defn test
+  ([]
+   (eftest/run-tests (eftest/find-tests "test")
+                     {:multithread? false}))
+  ([sym]
+   (eftest/run-tests (eftest/find-tests sym)
+                     {:multithread? false})))
+
+;;; DB access
+
+(defn db-run [f & args]
+  (apply f (:duct.database.sql/hikaricp system) args))
+
+;;; GraphQL
+
+(defn q [query-string]
+  (-> system
+      :aqoursql.graphql/schema
+      (lacinia/execute query-string nil nil)))
+
+;;; namespace settings
 
 (clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
 
---
+++ src/aqoursql/graphql.clj
@@ -0,0 +1,27 @@
+(ns aqoursql.graphql
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [com.walmartlabs.lacinia.pedestal :as lacinia]
+            [com.walmartlabs.lacinia.schema :as schema]
+            [com.walmartlabs.lacinia.util :as util]
+            [integrant.core :as ig]))
+
+(def resolver-map
+  {;; TODO: implement fetch-member-by-id resolver
+   :query/member-by-id (constantly nil)
+   ;; TODO: implement list-artist-members resolver
+   :Artist/members (constantly [])
+   ;; TODO: implement fetch-artist-by-id resolver
+   :Song/artist (constantly nil)})
+
+(defmethod ig/init-key ::schema
+  [_ _]
+  (-> (io/resource "aqoursql/graphql-schema.edn")
+      slurp
+      edn/read-string
+      (util/attach-resolvers resolver-map)
+      schema/compile))
+
+(defmethod ig/init-key ::service
+  [_ {:keys [schema options]}]
+  (lacinia/service-map schema options))
