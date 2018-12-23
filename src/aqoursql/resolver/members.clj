5a40e5127f7008085285e34554a13f011429a13b
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-12-24 01:48:57 +0900

    Complete fetch-member-by-id resolver implementation

--- src/aqoursql/resolver/members.clj
+++ src/aqoursql/resolver/members.clj
@@ -1,8 +1,5 @@
-(ns aqoursql.resolver.members)
+(ns aqoursql.resolver.members
+  (:require [aqoursql.boundary.db.member :as db.member]))
 
 (defn fetch-member-by-id [{:keys [db]} {:keys [id]} _]
-  (get {1 {:id 1
-           :name "高海 千歌"
-           :organization_id 1
-           :organization_name "浦の星女学院"}}
-       id))
+  (db.member/find-member-by-id db id))
